0a0556dea682b4126c8c179fe7aa82f720e21f76
Galen & Will <Galen & Will@Calcifer>
2015-11-10 11:12:03 -0800

    Can now reset and run another time! Also implemented automatic closing of StatWindow when game is reset.

--- src/paydaygame/Deck.java
+++ src/paydaygame/Deck.java
@@ -3,8 +3,12 @@
  */
 package paydaygame;
 
-/*More needs to go here, but: The reason Deck does not contain nextCard is that it would have been too hard to make it compute correctly, with different
-numbers of cards in different decks.*/
+/* Deck is the class from which Deal and Mail inherit most of their methods. Deck
+contains shuffle (currently using Fisher-Yates algoithm) and the actual decloration
+of the two 'decks'. Other than that, there's not much here yet. The reason Deck 
+does not contain nextCard is that it would have been too hard to make it compute
+correctly, with different numbers of cards in different decks. This may be changed
+in the future.*/
 
 import java.util.Random;
 
@@ -15,21 +19,27 @@ public class Deck {
     Mail toMailDeck;
     
     public Deck() {
-        
+        /*Deck is not constructed. It is just an idea that other classes refer
+        to. I bet there's a name for this...*/
     }
     
-     public void shuffle(int noOfCards, String deck) {
-        deckOrder = new int[noOfCards];
+     public void shuffle(int noOfCards, String deck) { /*Shuffle is a pretty 
+         complexe method. Let's take a look. Passed a number of cards for the 
+         deck, and also a name. The name is just for the print out at the end.*/
+        deckOrder = new int[noOfCards]; //Creates deckOrder.
         for (int i=0; i < noOfCards; i++) {
          deckOrder[i] = i;
-     }
+        }
         int end = noOfCards-1;
-        while (end > 0) {
-            int number = new Random().nextInt(end+1);
-            int temp = deckOrder[number];
-            deckOrder[number] = deckOrder[end];
-            deckOrder[end] = temp;
-            end--;
+        while (end > 0) { //This is the actual shuffle part.
+            int number = new Random().nextInt(end+1); /*Generates a new random number,
+            no greater than the number of cards in the deck.*/
+            int temp = deckOrder[number]; /*Creates a temp varible holding the number
+            which was occupying the slot in deckOrder.*/
+            deckOrder[number] = deckOrder[end]; /*Number at the end of the deckOrder
+            moved to the generated slot.*/
+            deckOrder[end] = temp; //Other number placed at end of deck.
+            end--; //End of deck reduced to eliminate moving of number just moved.
         }
         System.out.println("The " + deck + " Deck was shuffled!");
         //for (int i=0; i < 24; i++) {System.out.println(dealDeckOrder[i]);}
--- src/paydaygame/StatWindow.java
+++ src/paydaygame/StatWindow.java
@@ -3,11 +3,14 @@
  */
 package paydaygame;
 
+/*StatWindow opens a window on top of the initial GUI, which currently prints 
+out the Players' balences at the end of each month. Hopefully, one day, it will 
+print much more.*/
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
-import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumnModel;
@@ -19,7 +22,10 @@ public class StatWindow {
     JPanel toJPanel;
     JTable dataTable;
     
-    public StatWindow (int players, int months) {
+    public StatWindow (int players, int months) { /*This is the consturctor, which
+        is called from startGame in Interface. It sets up the window. I find most
+        J Components very cumbersome of introduce into a program, and so I like 
+        to build my own versions when I can. This was not one of those times.*/
         toJFrame = new JFrame("Statistics");
         toJPanel = new JPanel();
         toJFrame.add(toJPanel);
@@ -40,8 +46,15 @@ public class StatWindow {
         toJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
-    public void setValueAt(String value, int row, int column) {
+    public void setValueAt(String value, int row, int column) { /*This is a setter
+        called by printCash in Player.*/
         dataTable.setValueAt(value, row-1, column);
+    }
+    
+    public void close() {
+        System.out.println("The window was supposed to close.");
+        //toJFrame.dispatchEvent(new WindowEvent(toJFrame, WindowEvent.WINDOW_CLOSING));
+        toJFrame.dispose();
     }
     
 }
